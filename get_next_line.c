b1133e582dd7b6c0f101f6c4c0c7ad2915626a94
fgeslin <fgeslin@student.42quebec.com>
2022-11-04 16:47:03 -0400

    add print read

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,18 @@
 /*   By: fgeslin <fgeslin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 16:18:35 by fgeslin           #+#    #+#             */
-/*   Updated: 2022/11/04 16:18:36 by fgeslin          ###   ########.fr       */
+/*   Updated: 2022/11/04 16:44:56 by fgeslin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char	*buf;
+
+	printf("%d\n", BUFFER_SIZE);
+	buf = malloc(10000 * sizeof(char));
+	read(fd, buf, 10000);
+	return(buf);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,11 +6,22 @@
 /*   By: fgeslin <fgeslin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 16:18:33 by fgeslin           #+#    #+#             */
-/*   Updated: 2022/11/04 16:19:11 by fgeslin          ###   ########.fr       */
+/*   Updated: 2022/11/04 16:44:06 by fgeslin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# include <stdio.h> //DEBUG
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 0
+# endif
+
+char	*get_next_line(int fd);
 
 #endif
